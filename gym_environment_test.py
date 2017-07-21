474df53d8b4283d7e3dd065a8d6da631cf0c9065
Fangyi Zhi <fangyi.zhi@kbee.ag>
2017-07-21 17:24:16 +0200

    Implemented FrozenLake learning stuff

--- gym_environment_test.py
+++ gym_environment_test.py
@@ -1,15 +1,19 @@
 import gym
 from gym import envs
-print envs.registry.all()
+#print envs.registry.all()
 
-#env = gym.make('CartPole-v0')
-#for i_episode in range(20):
-#    observation = env.reset()
-#    for t in range(100):
-#        env.render()
-#        print(env.action_space.sample())
-#        action = env.action_space.sample()
-#        observation, reward, done, info = env.step(0)
-#        if done:
-#            print("Episode finished after {} timesteps".format(t+1))
-#            break
+env = gym.make('FrozenLake-v0')
+print env.action_space
+print env.observation_space
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(100):
+        #env.render()
+        action = env.action_space.sample()
+        observation, reward, done, info = env.step(action)
+        print "observation starts"
+        print observation
+        print "observation ends"
+        if done:
+            print("Episode finished after {} timesteps".format(t+1))
+            break
--- openAI_FrozenLake.py
+++ openAI_FrozenLake.py
@@ -10,11 +10,11 @@ from collections import Counter
 
 upload_folder = "/tmp/cartpole-experiment-2"
 LR = 1e-3
-env = gym.make('CartPole-v0')
+env = gym.make('FrozenLake-v0')
 env.reset()
 goal_steps = 300
-score_requirement = 90
-initial_games = 100000
+score_requirement = 1
+initial_games = 1000000
 
 def some_random_games_first():
     for episode in range(5):
@@ -34,32 +34,37 @@ def initial_population():
         score = 0
         game_memory = []
         prev_observation = []
+        first_step_flag = True
         for _ in range(goal_steps):
-            action = random.randrange(0, 2)    
+            action = env.action_space.sample()   
             observation, reward, done, info = env.step(action)
-
-            if len(prev_observation) > 0:
+            if first_step_flag == False:
                 game_memory.append([prev_observation, action])
 
             prev_observation = observation
+            first_step_flag = False
             score += reward
             if done:
                 break
         if score >= score_requirement:
             accepted_scores.append(score)
             for data in game_memory:
-                if data[1] == 1:
-                    output = [0, 1]
-                elif data[1] == 0:
-                    output = [1, 0]
+                if data[1] == 0:
+                    output = [1, 0, 0, 0]
+                elif data[1] == 1:
+                    output = [0, 1, 0, 0]
+                elif data[1] == 2:
+                    output = [0, 0, 1, 0]
+                elif data[1] == 3:
+                    output = [0, 0, 0, 1]
             
                 training_data.append([data[0], output])
 
         env.reset()
         scores.append(score)
 
-    training_data_save = np.array(training_data)
-    np.save('saved.npy', training_data_save)
+    #training_data_save = np.array(training_data)
+    #np.save('saved.npy', training_data_save)
 
     print('Average accepted score:', mean(accepted_scores))
     print('Median accepted score', median(accepted_scores))
@@ -85,52 +90,61 @@ def neural_network_model(input_size):
     network = fully_connected(network, 128, activation='relu')
     network = dropout(network, 0.8)
 
-    network = fully_connected(network, 2, activation='softmax')
+    network = fully_connected(network, 4, activation='softmax')
     network = regression(network, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
     model = tflearn.DNN(network, tensorboard_dir='log')
 
     return model
 
 def train_model(training_data, model=False):
-    X = np.array([i[0] for i in training_data]).reshape(-1, len(training_data[0][0]), 1)
+    X = np.array([i[0] for i in training_data]).reshape(len(training_data), 1, 1)
     y = [i[1] for i in training_data]
 
     if not model:
-        model = neural_network_model(input_size = len(X[0]))
+        model = neural_network_model(input_size = 1)
 
-    model.fit({'input':X}, {'targets':y}, n_epoch=3, snapshot_step=500, show_metric=True, run_id='openaistuff')
+    model.fit({'input':X}, {'targets':y}, n_epoch=5, snapshot_step=500, show_metric=True, run_id='openaistuff')
 
     return model
+
+def test_model(model):
+    scores = []
+    choices = []
+    for each_game in range(100):
+        score = 0
+        game_memory = []
+        prev_obs = []
+        env.reset()
+        first_step_flag = True
+        for _ in range(goal_steps):
+            #env.render()
+            if first_step_flag == True:
+                action = env.action_space.sample()
+            else:
+                action = np.argmax(model.predict([[[prev_obs]]])[0])
+            first_step_flag = False
+            choices.append(action)
+
+            new_observation, reward, done, info = env.step(action)
+            prev_obs = new_observation
+            game_memory.append([new_observation, action])
+            score += reward
+            if done:
+                break
+        scores.append(score)
+    return {'scores': scores, 'choices': choices}
+
 
 training_data = initial_population()
 model = train_model(training_data)
 
-scores = []
-choices = []
-env = wrappers.Monitor(env, upload_folder)
-for each_game in range(100):
-    score = 0
-    game_memory = []
-    prev_obs = []
-    env.reset()
-    for _ in range(goal_steps):
-        #env.render()
-        if len(prev_obs) == 0:
-            action = random.randrange(0, 2)
-        else:
-            action = np.argmax(model.predict(prev_obs.reshape(-1, len(prev_obs), 1))[0])
-        choices.append(action)
+result = test_model(model)
+#env = wrappers.Monitor(env, upload_folder)
 
-        new_observation, reward, done, info = env.step(action)
-        prev_obs = new_observation
-        game_memory.append([new_observation, action])
-        score += reward
-        if done:
-            break
-    scores.append(score)
-
+scores = result['scores']
+choices = result['choices']
 print('Average Score', sum(scores)/len(scores))
-print('Choice 1: {}, Choice 0: {}'.format(float(choices.count(1))/len(choices), float(choices.count(0))/len(choices)))
+print('Choice 0: {}, Choice 1: {}, Choice 2: {}, Choice 3: {}'.format(float(choices.count(0))/len(choices), float(choices.count(1))/len(choices), float(choices.count(2))/len(choices), float(choices.count(3))/len(choices)))
 
 #env.close()
 #gym.upload(upload_folder, api_key='sk_4mEnOlgJQeKLv6djQnww')
